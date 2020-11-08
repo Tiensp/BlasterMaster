a62ab8300ada21e0b575c01ed697c7564303e3b7
Tiensp <tiendr06@gmail.com>
2020-11-08 14:23:42 +0700

    Merge

---
+++ 05-ScenceManager/Camera.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "GameDefine.h"
+
+
+class CCamera
+{
+	static CCamera* __intance;
+
+	int xCam;
+	int yCam;
+
+	D3DXVECTOR2 camPos;
+
+	int width;
+	int height;
+
+	float boundWidth;
+	float boundHeight;
+	float boundLeft;
+	float boundRight;
+
+public:
+	static CCamera* GetInstance();
+	CCamera(int w, int h);
+	~CCamera();
+	D3DXVECTOR2 World2Render(const D3DXVECTOR2 &pos);
+	D3DXVECTOR2 Render2World(const D3DXVECTOR2& pos);
+	void SetPosition(float x, float y);
+	void SetDefaultCam();
+	float GetXCam();
+	float GetYCam();
+	int GetCurrentFloor();
+	int GetWidth();
+	int GetHeight();
+	RECT GetRectCam();
+	void SetBoundSize(float _w, float _h);
+	float GetBoundHeight();
+	float GetBoundWidth();
+	void SetBoundLeftRight(float _l, float _r);
+	void GetBoundLeftRight(float& _l, float& _r);
+	bool IsContainCam(RECT rect);
+	bool IsAutoCam();
+	void AutoCamX(float _distance, int direct);
+	void UpdateAutoCam(DWORD dt);
+	void LockCam();
+	void UnlockCam();
+};
+
+
+
---
+++ 05-ScenceManager/Map.cpp
@@ -0,0 +1,45 @@
+﻿#include "Map.h"
+#include "Utils.h"
+#include <fstream>
+
+CMap::CMap(int id, string fileMatrix, string fileTileSet)
+{
+	this->id = id;
+	LoadMatrix(fileMatrix);
+	LoadTileSet(fileTileSet);
+}
+
+CMap::~CMap()
+{
+}
+
+void CMap::LoadMatrix(string filePath)
+{
+	ifstream f(filePath, ios::in);
+	//Đọc hàng đầu tiên: kích thước tile, số cột, số dòng Matrix
+	f >> TileSize >> TileCols >> TileRows >> MapCols >> MapRows;
+	//Đọc matrix
+	Matrix = new int* [MapRows];
+	for (int i = 0; i <MapRows; i++) {
+		Matrix[i] = new int[MapCols];
+	}
+	for (int i = 0; i < MapRows; i++)
+		for (int j = 0; j < MapCols; j++)
+			f >> Matrix[i][j];
+	f.close();
+}
+
+void CMap::LoadTileSet(string filePath)
+{
+	
+	CTextures::GetInstance()->Add(static_cast<ObjectType>(id + MAPID), ToLPCWSTR(filePath), D3DCOLOR_XRGB(255, 255, 255));
+	TileTexture = CTextures::GetInstance()->Get(static_cast<ObjectType>(id + MAPID));
+
+}
+
+void CMap::DrawMap()
+{
+	CCamera* camera = CCamera::GetInstance();
+
+
+}
